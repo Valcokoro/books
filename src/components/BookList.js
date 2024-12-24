478816cf466bcbc26fd34bb00e2d78b12d25bbc8
Valentine Okoro <valcokoro@yahoo.com>
2024-12-24 11:45:46 +0000

    First Commit

---
+++ src/components/BookList.js
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import BooksContext from '../context/books';
+import BookShow from './BookShow'
+
+function BookList({books, onDelete, onEdit}){
+    
+    
+
+    const renderedBooks = books.map((book) => {
+        return <BookShow onDelete={onDelete} onEdit={onEdit} key={book.id} book={book} />
+    });
+
+    return <div className="book-list">
+        {renderedBooks}
+        </div>
+};
+
+export default BookList;
